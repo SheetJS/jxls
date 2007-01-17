53d8aaf86a06e59e36227f374fe15c9dd2dd48c6
mzarillo <mzarillo@0873f5a4-e61c-0410-97f5-bcdb7ec58e62>
2007-01-17 11:52:27 +0000

    -commented out code that was referencing old TagLib class
    git-svn-id: https://svn.code.sf.net/p/jxls/code/trunk@74 0873f5a4-e61c-0410-97f5-bcdb7ec58e62

--- jxls-core/src/main/java/net/sf/jxls/tag/TaglibXMLParser.java
+++ jxls-core/src/main/java/net/sf/jxls/tag/TaglibXMLParser.java
@@ -27,37 +27,38 @@ public class TaglibXMLParser {
     public TaglibXMLParser() {
     }
 
-    public Taglib parseTaglibXMLFile(String filename){
-        Taglib taglib = null;
-        try {
-            InputStream inputStream = loadTaglibDefinitionFile( filename );
-            taglib = parseTaglibXMLStream( inputStream );
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
-            e.printStackTrace();
-        }
-        return taglib;
-    }
+//    public TagLib parseTaglibXMLFile(String filename){
+//        TagLib taglib = null;
+//        try {
+//            InputStream inputStream = loadTaglibDefinitionFile( filename );
+//            taglib = parseTaglibXMLStream( inputStream );
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (SAXException e) {
+//            e.printStackTrace();
+//        }
+//        return taglib;
+//    }
 
     private InputStream loadTaglibDefinitionFile(String taglibFileName) {
         return getClass().getClassLoader().getResourceAsStream( taglibFileName );
     }
    
-    private Taglib parseTaglibXMLStream(InputStream inputStream) throws IOException, SAXException {
-        Digester digester = new Digester();
-        digester.setValidating( false );
-        digester.addObjectCreate( "", "");
-        Set tagKeys = Taglib.getTagMap().keySet();
-        for (Iterator iterator = tagKeys.iterator(); iterator.hasNext();) {
-            String tagKey = (String) iterator.next();
-            digester.addObjectCreate( TAGLIB_TAG, Taglib.class );
-            digester.addSetNext(TAGLIB_TAG + "/" + TAG_TAG, "addTag", "net.sf.jxls.tag.Tag");
-            digester.addSetNext( TAGLIB_TAG + "/" + TAG_TAG + "/" + ATTR_TAG, "addAttribute", "");
-//            digester.addSetProperties( TAGLIB_TAG );
-        }
-        Taglib taglib = (Taglib) digester.parse( inputStream );
-        return taglib;
-    }
+//    private TagLib parseTaglibXMLStream(InputStream inputStream) throws IOException, SAXException {
+//        Digester digester = new Digester();
+//        digester.setValidating( false );
+//        digester.addObjectCreate( "", "");
+//        
+//        Set tagKeys = TagLib.getTagMap().keySet();
+//        for (Iterator iterator = tagKeys.iterator(); iterator.hasNext();) {
+//            String tagKey = (String) iterator.next();
+//            digester.addObjectCreate( TAGLIB_TAG, TagLib.class );
+//            digester.addSetNext(TAGLIB_TAG + "/" + TAG_TAG, "addTag", "net.sf.jxls.tag.Tag");
+//            digester.addSetNext( TAGLIB_TAG + "/" + TAG_TAG + "/" + ATTR_TAG, "addAttribute", "");
+////            digester.addSetProperties( TAGLIB_TAG );
+//        }
+//        TagLib taglib = (TagLib) digester.parse( inputStream );
+//        return taglib;
+//    }
 
 }
