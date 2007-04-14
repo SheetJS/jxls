a952fa637ebd49a8128dcecad27882f65d0ef923
lvysochyn <lvysochyn@0873f5a4-e61c-0410-97f5-bcdb7ec58e62>
2007-04-14 12:17:20 +0000

    Breakdown of XLSBlockReader interface into XLSBLockReader, SimpleBlockReader and XLSLoopBlockReader interfaces
    git-svn-id: https://svn.code.sf.net/p/jxls/code/trunk@96 0873f5a4-e61c-0410-97f5-bcdb7ec58e62

--- jxls-reader/src/main/java/net/sf/jxls/reader/XLSBlockReader.java
+++ jxls-reader/src/main/java/net/sf/jxls/reader/XLSBlockReader.java
@@ -1,6 +1,5 @@
 package net.sf.jxls.reader;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -9,14 +8,13 @@ import java.util.Map;
  */
 public interface XLSBlockReader {
     void read(XLSRowCursor cursor, Map beans);
-    void setLoopBreakCondition(SectionCheck condition);
-    SectionCheck getLoopBreakCondition();
-    void addBlockReader(XLSBlockReader reader);
-    List getBlockReaders();
+
     int getStartRow();
+
     void setStartRow(int startRow);
+
     int getEndRow();
+
     void setEndRow(int endRow);
-    public void addMapping(BeanCellMapping mapping);
-    List getMappings();
+
 }
